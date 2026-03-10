bc9b984594ab6dfd18a8108f90f48d79e40a632b
Tharindu <tharinduonline1080@gmail.com>
2026-03-10 17:53:24 +0530

    Established database connection

---
+++ db.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  max: 10,
+});
+
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const pool = require("./db");
+
 const app = express();
-const port = 3000;
+app.use(express.json());
+
+const port = 5000;
+
+async function testDB() {
+  try {
+    const res = await pool.query("SELECT NOW()");
+    console.log("✅ Database connected");
+    console.log("Server time:", res.rows[0]);
+  } catch (err) {
+    console.error("❌ Database connection failed");
+    console.error(err.message);
+  }
+}
+
+testDB();
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
